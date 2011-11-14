50be85dc82e35fb7afc85e4469f97eb6158da582
Andrés Mejía <andmej@gmail.com>
2011-11-14 12:02:00 -0500

    EI login is working.

---
+++ api/app/models/interactiva/base.rb
@@ -0,0 +1,19 @@
+class Interactiva::Base
+  BASE_URL = "http://interactiva.eafit.edu.co/ei/"
+  
+  # Logs in to Interactive and returns the Mechanize agent or nil if couldn't log in.
+  def self.login!(username, password)
+    agent = Mechanize.new
+    page = agent.get(BASE_URL + "ingresar.do")
+    login_form = page.form("loginForm")
+    login_form.username = username
+    login_form.password = password
+    page = agent.submit(login_form, login_form.buttons.first)
+    if page.root.text =~ /Bienvenido/
+      agent
+    else
+      raise AuthenticationException.new("Invalid username or password")
+    end
+  end
+  
+end
